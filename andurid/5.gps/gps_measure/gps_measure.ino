6dd1d9d2f549d9bb936a0b58e7904ad81ea087b5
Juljus <juljus@gmail.com>
2024-02-21 11:01:05 +0200

    Update gps_measure.ino

--- andurid/5.gps/gps_measure/gps_measure.ino
+++ andurid/5.gps/gps_measure/gps_measure.ino
@@ -1,64 +1,81 @@
-// https://www.circuitbasics.com/how-to-setup-a-gps-sensor-on-the-arduino/
+/**
+ * Main:        gps_measure.ino
+ * Autor:       Juljus
+ * Loodud:      20.02.2024
+ * Muudatud:    21.02.2024
+ *
+ * Selgitus:    https://www.circuitbasics.com/how-to-setup-a-gps-sensor-on-the-arduino/ põhjal
+ * 
+ * Teeked:      - SoftwareSerial
+ *              - Adafruit_GPS
+ * 
+ * TODO:        gps start delay of 1000 ms
+ *              
+ */
 
+
+// import
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 
+// globaalsed muutujad
 SoftwareSerial gpsSerial(3, 2); // set pin 3 as RX and 3 as TX
 Adafruit_GPS GPS(&gpsSerial); // connect adafruitGPS library to the new serial monitor (gpsSerial)
-
 char c;
 
 void setup() {
-  Serial.begin(9600);
-  GPS.begin(9600);
+  Serial.begin(115200);
+  GPS.begin(115200);
 
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // only recieve GPRMC and GPGGA sentences
 
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // set recieve frequency to 1hz
-  delay(1000);
 }
 
 void loop() {
 
   clearGPS(); // clear out the serial buffer
 
-  // wait until a NMEA sentence is ready to be read
-  while (!GPS.newNMEAreceived()) {
+  // wait until a NMEA sentence is ready to be read and the gps start delay is over
+  if (GPS.newNMEAreceived()) {
+
+    GPS.parse(GPS.lastNMEA()); // received and parse the last NMEA sentence
+
+    // print 1 if the gps has a fix, 0 if not
+    Serial.print(millis());
+    Serial.print(",");
+    Serial.print("gpsFix");
+    Serial.print(",");
+    Serial.println(GPS.fix);
+
+    // in case of a fix
+    if (GPS.fix) {
+
+      // Location in degrees
+      // Serial.print("Google Maps location: ");
+      // Serial.print(GPS.latitudeDegrees, 4);
+      // Serial.print(", ");
+      // Serial.println(GPS.longitudeDegrees, 4);
+
+      // Speed
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.print("gpsSpeed"); // in km/h
+      Serial.print(",");
+      Serial.println(GPS.speed * 1.85); // convert from knots to km/h before printing
+
+      // Altitude
+      // Serial.print(millis());
+      // Serial.print(",");
+      // Serial.print("altitude");
+      // Serial.print(",");
+      // Serial.println(GPS.altitude);
+    }
+
+  }
+  else {
     c = GPS.read();
   }
-
-  GPS.parse(GPS.lastNMEA()); // received and parse the last NMEA sentence
-
-  // print 1 if the gps has a fix, 0 if not
-  Serial.print("Fix: ");
-  Serial.print(GPS.fix);
-
-  // in case of a fix
-  if (GPS.fix) {
-
-    // Location in longitude/latitude
-    Serial.print("Location: ");
-    Serial.print(GPS.latitude, 4);
-    Serial.print(GPS.lat);
-    Serial.print(", ");
-    Serial.print(GPS.longitude, 4);
-    Serial.println(GPS.lon);
-
-    // Location in degrees
-    Serial.print("Google Maps location: ");
-    Serial.print(GPS.latitudeDegrees, 4);
-    Serial.print(", ");
-    Serial.println(GPS.longitudeDegrees, 4);
-
-    // Speed
-    Serial.print("Speed (km/h): ");
-    Serial.println(GPS.speed * 1.85); // convert speed from knots to km/h
-
-    // Altitude
-    Serial.print("Altitude: ");
-    Serial.println(GPS.altitude);
-  }
-}
 
 // take a reading from the GPS, parse the sentence, and then do nothing with it
 void clearGPS() {
