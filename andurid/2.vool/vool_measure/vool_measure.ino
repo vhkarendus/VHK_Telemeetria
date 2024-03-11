f3b9f5688ee0b1df12398ede9b8b8c16935dde95
KLaidma <148231957+KLaidma@users.noreply.github.com>
2024-03-11 15:01:13 +0200

    Tegin voolutugevuse jaoks koodi

---
+++ andurid/2.vool/vool_measure/vool_measure.ino
@@ -0,0 +1,64 @@
+/*
+const int analogPin = A0; 
+const float voltageRef = 5.0; 
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(analogPin);
+  float voltage = (sensorValue / 1023.0) * voltageRef; // muudab analog väärtus voltideks
+  float current = ((voltage - (voltageRef / 2.0)) / 0.185); //muudab voldid ampriteks (ACS712 formula)
+
+  Serial.print("Current: ");
+  Serial.print(current);
+  Serial.println(" mA");
+
+  delay(1000); // Delay for 1 second
+}
+/*
+/**
+ * Main:        arduinoSkriptMall.ino
+ * Autor:       Francisco Javier Ortín Cervera
+ * Loodud:      09.02.2024
+ * Muudatud:    09.02.2024
+ *
+ * Selgitus:    See on arduino skript malli lühike kirjeldus, peamine info peab olema README failis:
+ *              Selleks, et meil oleks nii ühtlane kui võimalik: 
+ *              - Funktsiooni nimed peavad olema camelCase-is (ehk iga sõna algus on suur täht)
+ *              - Funktsiooni nimed kirjutame inglise keeles, aga kommentaarid võid eesti keeles
+ * 
+ * Teeked:
+ * 
+ * TODO:        - Kui vaja võib olla kasuks lisada, mida järgmisena teha
+ *              
+ */
+
+
+ // Importimised
+
+ // Globaalsed muutujad
+const int Ammeter = A0;
+const float arduvoltage = 5.0; 
+ 
+void setup(){
+  Serial.begin(115200);
+  pinMode(Ammeter, INPUT);
+}
+
+void loop(){
+  // Anduri kood
+  int sensorValue = analogRead(Ammeter);
+  float voltage = (sensorValue / 1023.0) * arduvoltage; // muudab analog väärtuse voltideks
+  float current = ((voltage - (arduvoltage / 2.0)) / 0.01); //muudab voldid ampriteks (ACS712 formula)
+
+  delay(1000); // Delay for 1 second
+  // Kui anduri andmed on käes tuleb saata jadaside kaudu csv formaadis:
+  // timestamp,andmeNimi,andmed
+  Serial.print(millis());
+  Serial.print(",");
+  Serial.print("Ammeter"); // siin oleks rpm, ta, tm, vool, pingeA1, pingeA2, gps
+  Serial.print(",");
+  Serial.println(current);
+}
