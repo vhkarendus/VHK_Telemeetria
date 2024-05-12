e8ffad45e963c4927e5807c48e9fa154404ec9c8
Juljus <juljus@gmail.com>
2024-05-12 21:07:09 +0300

    update

--- andurid/4.temp/dev_temp.ino
+++ andurid/4.temp/dev_temp.ino
@@ -1,28 +1,38 @@
- // Globaalsed muutujad
-int aku_andur = A0;
-int mootori_andur = A1;
-int Vout;
-int Vout_2;
+const int thermistor_pin = A2;
+const int thermistor_extra_resistor = 5100;
+const int thermistor_base_resistance = 3300;
+const int thermistor_B_value = 3977;
+const int thermistor_base_temperature = 298.15; // 25C in Kelvin
+const float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
-void setup(){
-  Serial.begin(115200);
-  pinMode(aku_andur, INPUT);
-  pinMode(mootori_andur, INPUT);
+int thermistor_extra_resistor_out;
+float temperature, temperature_C, thermistor_resistance;
+
+void setup() {
+Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
 
-  //aku temperatuuri anduri kontrollimine
-  Vout = analogRead(aku_andur);
-  
-  if(Vout<0 || Vout>1023){
-  Serial.println("Aku temperatuuri andur on katki!");
-  }
+  thermistor_extra_resistor_out = analogRead(thermistor_pin);
+  thermistor_resistance = thermistor_extra_resistor * (1023.0 / (float)thermistor_extra_resistor_out - 1.0);
 
-  //mootori temperatuuri anduri kontrollimine
-  Vout_2 = analogRead(mootori_andur);
-  
-  if(Vout_2<0 || Vout_2>1023){
-  Serial.println("Mootori temperatuuri andur on katki!");
-  }
+
+  temperature = thermistor_resistance / thermistor_base_resistance;
+  temperature = log(temperature);
+  temperature /= thermistor_B_value;
+  temperature += 1 / (thermistor_base_temperature + 273.15);
+  temperature = 1 / temperature;
+
+  temperature_C = temperature - 273.15;
+
+  // temperature = 1.0 / ((1 / thermistor_base_temperature) + ((log(thermistor_resistance)) / thermistor_B_value));
+  // temperature_C = temperature - 273.15;
+
+
+  Serial.print("Temperature: "); 
+  Serial.print(temperature_C);
+  Serial.println(" C"); 
+
+  delay(500);
 }
